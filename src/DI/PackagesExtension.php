06ace3007c0fa759d5279e6254ceadfd7a923c6a
Michal Vysinsky <vysinsky@live.com>
2014-04-19 14:56:07 +0200

    Remove custom compiler extension class

--- src/DI/PackagesExtension.php
+++ src/DI/PackagesExtension.php
@@ -9,37 +9,23 @@
 namespace AnnotateCms\Packages\DI;
 
 
-use AnnotateCms\Framework\DI\CompilerExtension;
 use AnnotateCms\Packages\Loaders\AssetsLoader;
 use AnnotateCms\Packages\Loaders\PackageLoader;
 use Kdyby\Events\DI\EventsExtension;
+use Nette\DI\CompilerExtension;
 
 class PackagesExtension extends CompilerExtension
 {
-
-    function getServices()
+    public function loadConfiguration()
     {
-        return [
-            "packageLoader" => [
-                "class" => PackageLoader::classname,
-                "tags" => [EventsExtension::SUBSCRIBER_TAG],
-            ],
-            "assetsLoader" => [
-                "class" => AssetsLoader::classname,
-                "tags" => [EventsExtension::SUBSCRIBER_TAG],
-            ],
-        ];
-    }
+        $builder = $this->getContainerBuilder();
 
+        $builder->addDefinition($this->prefix('packageLoader'))
+            ->setClass(PackageLoader::classname)
+            ->addTag(EventsExtension::SUBSCRIBER_TAG);
 
-    function getFactories()
-    {
-        // TODO: Implement getFactories() method.
-    }
-
-
-    function  getDefaults()
-    {
-        // TODO: Implement getDefaults() method.
+        $builder->addDefinition($this->prefix('assetsLoader'))
+            ->setClass(AssetsLoader::classname)
+            ->addTag(EventsExtension::SUBSCRIBER_TAG);
     }
 }
